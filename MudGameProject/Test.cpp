04c36c73b41c602257eadb120790a9f96b9c0244
_tireDep <wing997@naver.com>
2020-06-02 15:31:39 +0900

    [프로토타입 제작 및 테스트]
    - 방향키 입력시 블록 이동

--- MudGameProject/Test.cpp
+++ MudGameProject/Test.cpp
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<conio.h>	// _getch()
-#include<stdlib.h>
 #include<time.h>
+#include<stdlib.h>
 
 #include<Windows.h>
 
 #define ARRSIZE 4
+enum MovePos { rightMove = 1, leftMove = 1, upMove = 1, downMove = 1 };
+
 
 void PrintArr(int(*mapArr)[ARRSIZE]);
 
 int main()
 {
-	system("mode con cols=80 lines=25");
-	int posX = 0, posY = 0, randVal = 0;
+	system("mode con cols=50 lines=25");
+
 	srand((unsigned)time(NULL));
+	int posX = 0, posY = 0, randVal = 0;
 	int mapArr[ARRSIZE][ARRSIZE] = { 0 };
 
 	int check = 0;
-	while (1)
+/*	while (1)
 	{
 		check++;
 		posX = rand() % ARRSIZE;
@@ -40,9 +43,27 @@ int main()
 		if (check > 1)
 			break;
 	}
+	*/
+	mapArr[0][0] = 2;
+	mapArr[1][0] = 2;
+	mapArr[2][0] = 2;
+	mapArr[3][0] = 2;
+	mapArr[1][1] = 2;
+	mapArr[3][1] = 2;
+
+	mapArr[3][2] = 2;
+	mapArr[3][3] = 2;
+	PrintArr(mapArr);
 	
 	// 키 입력 구분
 	// 방향키는 2바이트기 때문에 리턴값이 두번
+
+	/*
+	 0, 0 /  0, -1  /  0, 0
+	-1, 0 /  0, 0   / +1, 0
+	 0, 0 /  0, +1  /  0, 0
+	*/
+
 	int userInput = 0;
 	userInput = _getch();
 	if (userInput == 224)
@@ -52,10 +73,77 @@ int main()
 		{
 		case 72:
 			printf("up\n");
+			
+			/*for (int i = upMove; i < ARRSIZE; i++)
+			{
+				for (int j = 0; j < ARRSIZE; j++)
+				{
+					if (mapArr[i][j] != 0 && i<ARRSIZE && j<ARRSIZE)
+					{
+						mapArr[i - upMove][j] = mapArr[i][j];
+						mapArr[i][j] = 0;
+					}
+				}
+			}*/
+
+			for (int posX = 0; posX < ARRSIZE; posX++)
+			{
+				for (int posY = 0; posY < ARRSIZE; posY++)
+				{
+					if (posY == 0)
+						continue;
+
+					if (mapArr[posY][posX] != 0)	// 값이 있는 경우
+					{
+						if (mapArr[posY - upMove][posX] == 0)	// 앞 칸이 비었을 경우
+						{
+							mapArr[posY - upMove][posX] = mapArr[posY][posX];
+							mapArr[posY][posX] = 0;
+							posY = 0;
+							continue;
+						}
+						else if (mapArr[posY - upMove][posX] == mapArr[posY][posX])	// 앞 칸의 숫자와 현재 칸의 숫자가 같을 경우
+						{
+							mapArr[posY - upMove][posX] = -(2 * mapArr[posY][posX]);
+							mapArr[posY][posX] = 0;
+							posY--;
+							continue;
+						}
+					}
+				}
+			} // for()
 			break;
 
 		case 80:
 			printf("down\n");
+
+			for (int posX = 0; posX < ARRSIZE; posX++)
+			{
+				for (int posY = ARRSIZE; posY >= 0; posY--)
+				{
+					if (posY >= ARRSIZE - downMove)
+						continue;
+
+					if (mapArr[posY][posX] != 0)	// 값이 있는 경우
+					{
+						if (mapArr[posY + downMove][posX] == 0)	// 앞 칸이 비었을 경우
+						{
+							mapArr[posY + downMove][posX] = mapArr[posY][posX];
+							mapArr[posY][posX] = 0;
+							posY = ARRSIZE;
+							continue;
+						}
+						else if (mapArr[posY + downMove][posX] == mapArr[posY][posX])	// 앞 칸의 숫자와 현재 칸의 숫자가 같을 경우
+						{
+							mapArr[posY + downMove][posX] = -(2 * mapArr[posY][posX]);
+							mapArr[posY][posX] = 0;
+							posY++;
+							continue;
+						}
+					}
+				}
+			} // for()
+
 			break;
 
 		case 75:
@@ -69,6 +157,10 @@ int main()
 		default:
 			break;
 		}
+	}
+	else
+	{
+		printf("else Key\n");
 	}
 	/*
 	참고 아스키 코드
