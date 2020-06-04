e5d2657d935ac4f9dfb98fd6b8d0190eb1e2dd7d
_tireDep <wing997@naver.com>
2020-06-04 15:53:07 +0900

    [게임화면 제작]
    - 입력 체크 추가
    
    - 최대값 생성시 게임 종료 조건 추가
    
    - 일시정지, 종료 조건 및 옵션 변경
    -> 사용자가 입력한 값에 따라 이어하기, 새로하기, 종료하기로 구현

--- MudGameProject/GameScreenHeader.h
+++ MudGameProject/GameScreenHeader.h
@@ -1,10 +1,10 @@
 #pragma once
 
-int Update();
+int Update(int *checkPlay);
 void SetNewNum(int(*mapArr)[BASICARRSIZE], int check);
-void PlayerInput(int(*mapArr)[BASICARRSIZE], int *checkCanMove, int *checkGameOver, int *kbhitCnt);
+void PlayerInput(int(*mapArr)[BASICARRSIZE], int *checkCanMove, int *checkGameOver, int *checkMaxNum, int *kbhitCnt);
 
-bool IsCanMove(int(*mapArr)[BASICARRSIZE], int posY, int posX, int *checkCanMove, int *checkGameOver, int moveY, int moveX);
+bool IsCanMove(int(*mapArr)[BASICARRSIZE], int posY, int posX, int *checkCanMove, int *checkGameOver, int *checkMaxNum, int moveY, int moveX);
 void MoveToInput(int(*mapArr)[BASICARRSIZE], int *posY, int *posX, int moveY, int moveX, int setStart, int setNext, int moveTo);
 
 void SetAbsVal_UpDown(int(*mapArr)[BASICARRSIZE], int posX);
@@ -13,8 +13,10 @@ void SetAbsVal_RightLeft(int(*mapArr)[BASICARRSIZE], int posY);
 void PrintArr(int(*mapArr)[BASICARRSIZE], char *string, int *kbhitCnt);
 void TextColor(int foreground, int background);
 
-bool CheckGameContinue(int(*mapArr)[4], int *kbhitCnt);
+bool CheckGameContinue(int(*mapArr)[4], int *kbhitCnt, int *checkPlay);
 void PrintNewInput(int(*mapArr)[4], char *string, int *kbhitCnt);
 
 void DeleteInput(int *kbhitCnt);
 void CheckKbhitCnt(int *kbhitCnt);
+
+// int checkMaxNum = 0;
--- MudGameProject/Main.cpp
+++ MudGameProject/Main.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
+	int checkPlay = 0;
 	srand((unsigned)time(NULL));
-	system("mode con cols=39 lines=37");
+	system("mode con cols=39 lines=39");
 
 	// todo/Maybe : bgm 추가, 장애물 or 맵 크기 확장
 	// todo : 메인화면 및 튜토리얼
@@ -12,7 +13,27 @@ int main()
 	// todo
 	// - 만들 수 있는 최고 숫자일 때, 게임 종료
 	// - 점수 계산, 최고점 계산
-	Update();
+	
+	Update(&checkPlay);
+	while (checkPlay >= 0)
+	{
+		if (checkPlay == 50)
+		{
+			printf("2\n\n>> ReStart Game\n\nReady to New Game : ");
+			for (int i = 5; i > 0; i--)
+			{
+				Sleep(500);
+				printf("%d ", i);
+			}
+			checkPlay = 0;
+			Update(&checkPlay);
+		}
+		else
+		{
+			puts("AnyKey\n\n>> Game Exit\n");
+			return 0;
+		}
+	}
 
 	// todo : 점수 계산 및 스코어링// 미정
 }
