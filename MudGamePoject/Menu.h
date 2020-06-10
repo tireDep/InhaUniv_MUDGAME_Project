29d435ff416f69cc890398e3f223ee82bd0839a2
UJae Im <wnddpd01@naver.com>
2020-06-10 16:30:05 +0900

    Modify Menu

---
+++ MudGamePoject/Menu.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "BasicHeader.h"
+
+void GotoXY(int x, int y);
+void TextColor(int foreground, int background);
+BYTE SelectMenu(BYTE initX, BYTE initY, BYTE gap, BYTE maxMenu, BYTE menuString[][20]);
+void EraseMenu(BYTE initX, BYTE initY, BYTE gap, BYTE maxMenu, BYTE menuString[][20]);
